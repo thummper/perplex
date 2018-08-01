67069bcd55097c3a183954385390f2f0a6effe70
Thummper <thummper@hotmail.co.uk>
2018-08-01 12:41:02 +0100

    Addded some homepage content
    Homepage will display most recent 3 posts and games with links to posts/games page.
    
    I'll pull the game code from thummper.net and adjust the links there.

--- Perplex/page-home.php
+++ Perplex/page-home.php
@@ -3,9 +3,20 @@
 $context = Timber::get_context();
 wp_enqueue_style( 'pplx-style', get_stylesheet_uri() );
 $context['header_image'] = get_site_url() . "/wp-content/themes/Perplex/assets/tilt-min.jpg";
-$context['post'] = new Timber\Post();
 
-//Get all job types and give them to the page. 
+
+//Get first 3 games and posts
+
+$args = array(
+    'post_type' => 'games',
+    'posts_per_page' => 3,
+);
+$context['games'] = Timber::get_posts($args);
+$args = array(
+    'post_type' => 'post',
+    'posts_per_page' => 4
+);
+$context['posts'] = Timber::get_posts($args);
 
 
 
